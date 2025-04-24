50fa398ce0fb58cbe1347e2f7b80d4e9e19b7ce5
ziguo1 <ziguo1@s.sfusd.edu>
2025-04-24 05:46:42 +0000

    that was light work wtf

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,14 @@
 public class APLine
 {
   /* your code here */
-  
+  protected double a, b, c;
+
+  public APLine(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public double getSlope() { return -a / b; }
+  public boolean isOnLine(double x, double y) { return (a * x) + (b * y) + c == 0; } 
 }
